8765965cf828fd81747e128fd98c7b5d23310bb8
hrink <145516079+hrink@users.noreply.github.com>
2023-10-05 16:54:19 +0900

    [BOJ] 11659번: 구간 합 구하기 1

---
+++ 003번.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[100005] ;
+int s[100005];
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, m;
+
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+		s[i] = s[i - 1] + arr[i];
+	}
+	for (int i = 0; i < m; i++) {
+		int a, b;
+		cin >> a >> b;
+		cout << s[b] - s[a - 1] << '\n';
+	}
+}
