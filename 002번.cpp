336bbfdd0018152409a2f755ed6fe92deb56dd9a
hrink <145516079+hrink@users.noreply.github.com>
2023-10-05 16:42:54 +0900

    [BOJ] 1546번: 평균 구하기

---
+++ 002번.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n, sum = 0, max = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		if (max < num) max = num;
+		sum += num;
+	}
+	double result = sum * 100.0 / max / n;
+	cout << result;
+}
